d71dfe73b63b77e73e79692f9782c668fd72f02f
superwubj <1075178181@qq.com>
2017-09-19 09:19:49 +0800

    添加分润统计

--- pages/analyse/bonus/bonus.js
+++ pages/analyse/bonus/bonus.js
@@ -1,10 +1,67 @@
 // pages/analyse/bonus/bonus.js
+// 在需要使用的js文件中，导入js  
+var util = require('../../../utils/util.js'); 
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+
+    yinlian: '81.21',//银联
+    wx: '66.33',//微信
+    zfb: '48.75',//支付宝
+    qt: '23.71',//其他
+
+    changeNum:0,
+    date:'',
+
+    bouns:[
+      {
+        date:'09-18',
+        yinlian:'85.32',
+        wx:'85.32',
+        zfb:'32.32',
+        qt:'32.42'
+      }, {
+        date: '09-17',
+        yinlian: '85.32',
+        wx: '85.32',
+        zfb: '32.32',
+        qt: '32.42'
+      }, {
+        date: '09-16',
+        yinlian: '85.32',
+        wx: '85.32',
+        zfb: '32.32',
+        qt: '32.42'
+      }, {
+        date: '09-15',
+        yinlian: '85.32',
+        wx: '85.32',
+        zfb: '32.32',
+        qt: '32.42'
+      }, {
+        date: '09-14',
+        yinlian: '85.32',
+        wx: '85.32',
+        zfb: '32.32',
+        qt: '32.42'
+      }, {
+        date: '09-13',
+        yinlian: '85.32',
+        wx: '85.32',
+        zfb: '32.32',
+        qt: '32.42'
+      }, {
+        date: '09-12',
+        yinlian: '85.32',
+        wx: '85.32',
+        zfb: '32.32',
+        qt: '32.42'
+      }
+    ],
   
   },
 
@@ -19,7 +76,12 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
+    var dateTime = util.changeMonth(new Date(), 3, this.data.changeNum);
+    this.setData({
+      date:dateTime
+    })
+
   },
 
   /**
@@ -62,5 +124,37 @@ Page({
    */
   onShareAppMessage: function () {
   
+  },
+  back:function(){
+    var newDate = this.chaifen();
+    console.log(newDate);
+    var dateTime = util.changeMonth(newDate,0);
+    this.setData({
+      date: dateTime
+    })
+  },
+  next:function(){
+    var newDate = this.chaifen();
+    console.log(newDate);
+    var dateTime = util.changeMonth(newDate, 1);
+    this.setData({
+      date: dateTime
+    })
+  },
+  chaifen:function(){
+    var that = this;
+    var strs = new Array();
+    strs = that.data.date.split('年');
+    var month = strs[1];
+    var months = new Array();
+    months = month.split('月');
+    var newYear = strs[0];
+    var newMonth = months[0];
+    // var timestamp2 = Date.parse(new Date(newYear + '-' + newMonth));
+    console.log(newYear, newMonth);
+    var newDate = new Date(newYear,newMonth);
+    // newDate.setTime(timestamp2);
+    console.log(newDate);
+    return newDate;
   }
 })
--- utils/util.js
+++ utils/util.js
@@ -22,11 +22,35 @@ function formatTime(date,style) {
 
 }
 
+function changeMonth(date,changeStyle,changeNum){
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  if (changeStyle == 0){
+    var newMonth = month - 1
+    if (newMonth == 0){
+      return (year - 1) + '年12月'  
+    }else{
+      return year + '年' + newMonth + '月'
+    }
+  } else if (changeStyle == 1)
+  {
+    var newMonth = month + 1
+    if (newMonth == 13) {
+      return (year + 1) + '年01月'
+    } else {
+      return year + '年' + newMonth + '月'
+    }
+  }else{
+    return year + '年' + month + '月'
+  }
+}
+
 function formatNumber(n) {
   n = n.toString()
   return n[1] ? n : '0' + n
 }
 
 module.exports = {
-  formatTime: formatTime
+  formatTime: formatTime,
+  changeMonth:changeMonth
 }
